a9f5191e733dd716fa861fea69e63d7fa7ae24dd
F776 <fern.belmar@duocuc.cl>
2024-07-06 01:45:43 -0400

    página terminada

--- app/admin/dashboard/pedidos/[id]/view/not-found.tsx
+++ app/admin/dashboard/pedidos/[id]/view/not-found.tsx
@@ -1,17 +1,17 @@
-import Link from 'next/link';
-import { FaceFrownIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
 
 export default function NotFound() {
   return (
     <main className="flex h-full flex-col items-center justify-center gap-2">
       <FaceFrownIcon className="w-10 text-gray-400" />
       <h2 className="text-xl font-semibold">404 Not Found</h2>
-      <p>Could not find the requested invoice.</p>
+      <p>No se pudo encontrar el pedido.</p>
       <Link
-        href="/admin/dashboard/invoices"
+        href="/admin/dashboard/pedidos"
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
       >
-        Go Back
+        Regresar
       </Link>
     </main>
   );
