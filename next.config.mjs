5cff335e43d29995878facd3819fd99be384e91a
f776 <fern.belmar@duocuc.cl>
2025-09-07 17:30:04 -0300

    feat: modify dockerfile

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
-     {
+      {
         protocol: "https",
         hostname: "picsum.photos",
         port: "",
         pathname: "/**",
       },
-        {
+      {
         protocol: "https",
         hostname: "img.clerk.com",
         port: "",
