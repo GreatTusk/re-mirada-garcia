ab03727288da8707c47d38e3e86adb147623c156
f776 <fern.belmar@duocuc.cl>
2025-09-07 16:53:06 -0300

    fix: use static json instead of failing request (for some unknown reason)

--- app/(main)/portafolio/page.tsx
+++ app/(main)/portafolio/page.tsx
@@ -7,7 +7,49 @@ async function fetchItemPortafolio() {
   const res = await fetch(
     `${process.env.BACKEND_URL}/api/itemportafolio/?format=json`,
   );
-  return await res.json();
+
+  const data: ItemPortafolio[] = await res.json();
+
+  return JSON.parse(
+    "[\n" +
+      "  {\n" +
+      '    "images_config": {\n' +
+      '      "id": 1,\n' +
+      '      "image_folder": {\n' +
+      '        "folder": "/img/fotos-concierto"\n' +
+      "      },\n" +
+      '      "width": 1908,\n' +
+      '      "height": 1272\n' +
+      "    },\n" +
+      '    "titulo": "Vibraciones Inmortalizadas",\n' +
+      '    "descripcion": "¡Transmite la energía de tus conciertos con imágenes impactantes! Soy un fotógrafo especializado en eventos musicales, capturo la pasión de tu música en cada actuación en vivo. Con años de experiencia, garantizo que cada foto capture la esencia única de tu presentación. Contáctame para llevar tu presencia en el escenario al siguiente nivel."\n' +
+      "  },\n" +
+      "  {\n" +
+      '    "images_config": {\n' +
+      '      "id": 2,\n' +
+      '      "image_folder": {\n' +
+      '        "folder": "/img/fotos-matrimonio"\n' +
+      "      },\n" +
+      '      "width": 1920,\n' +
+      '      "height": 1280\n' +
+      "    },\n" +
+      '    "titulo": "Tu día más especial",\n' +
+      '    "descripcion": "Bienvenido a la galería del matrimonio de Juan y María. Cada imagen cuenta una historia, un momento lleno de amor y emoción. Desde los votos hasta el baile, cada detalle es inmortalizado con pasión y creatividad. Permíteme ser parte de tu día especial y contar tu historia a través de mi lente."\n' +
+      "  },\n" +
+      "  {\n" +
+      '    "images_config": {\n' +
+      '      "id": 3,\n' +
+      '      "image_folder": {\n' +
+      '        "folder": "/img/fotos-retratos"\n' +
+      "      },\n" +
+      '      "width": 1920,\n' +
+      '      "height": 1280\n' +
+      "    },\n" +
+      '    "titulo": "Captura tu esencia",\n' +
+      '    "descripcion": "¿Buscas un retrato que capture tu verdadera esencia? Con años de experiencia y un enfoque dedicado a la excelencia, garantizo una sesión de fotografía personalizada que resalte lo mejor de ti. Cada imagen será una obra de arte atesorada para siempre. ¡Contáctame hoy y deja que inmortalice tu belleza!"\n' +
+      "  }\n" +
+      "]",
+  );
 }
 
 async function fetchItemTestimonio() {
